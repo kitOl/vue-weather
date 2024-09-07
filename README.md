d21b00d9b96b9a0c6d7c20246f565e2b14e45cf2
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-07 23:34:30 +0300

    install and configure tailwind and added Nav

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # vue-weather
 
-This template should help get you started developing with Vue 3 in Vite.
+```
+npm i -D tailwindcss postcss autoprefixer
+```
+
+and configurate tailwind:
+
+```
+npx tailwindcss init -p
+```
 
 ## Recommended IDE Setup
 
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
-<script>
+<script setup>
 import { RouterView } from 'vue-router';
-export default {};
+import SiteNavigation from './components/SiteNavigation.vue';
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
