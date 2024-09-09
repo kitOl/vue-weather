4feac107221ad5e225d0edf10ca02cf941b9e310
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-09 18:31:29 +0300

    added retrieve data from local storage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,6 +28,11 @@
         </template>
       </ul>
     </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList />
+      </Suspense>
+    </div>
   </main>
 </template>
 
@@ -35,6 +40,7 @@
 import { ref } from 'vue';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
+import CityList from '../components/CityList.vue';
 
 const router = useRouter();
 const previewCity = (searchResult) => {
